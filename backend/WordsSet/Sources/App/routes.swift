f20653d5a607f2833433ddf385f9674a2ed26704
Adam Roe <adam.j.roe@gmail.com>
2019-05-05 16:32:56 +0200

    add snarky comments

--- backend/WordsSet/Sources/App/routes.swift
+++ backend/WordsSet/Sources/App/routes.swift
@@ -1,10 +1,11 @@
 import Vapor
 
-/// Register your application's routes here.
+// all of my many routes
 public func routes(_ router: Router) throws {
 
+    // a super sophistacted controller
     let wordController = WordController()
-    router.get("/", use: wordController.index)
-    router.post("word", use: wordController.check)
+    router.get("/", use: wordController.index) //placeholder for server-side rendering for now
+    router.post("word", use: wordController.check) //process post request from frontend
 
 }
