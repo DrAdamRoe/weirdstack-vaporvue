a4e4834d3fac6053418d4615dac67542feb9bfa1
Adam Roe <adam.j.roe@gmail.com>
2019-05-05 16:31:22 +0200

    move post processing to controller

--- backend/WordsSet/Sources/App/Controllers/WordController.swift
+++ backend/WordsSet/Sources/App/Controllers/WordController.swift
@@ -19,5 +19,19 @@ final class WordController {
         
         return httpResponse
         }
+    	
+    
+    func check(_ req: Request) throws -> Future<HTTPResponse>{
+        return try req.content.decode(WordRequest.self).map(to: HTTPResponse.self){
+            WordRequest in print(WordRequest.word)
+            
+        var httpResponse = HTTPResponse(status: .ok)
+        let response_body = "Thanks for the word: " + WordRequest.word
+        httpResponse.body = HTTPBody(string: response_body)
+        httpResponse.contentType = .html
+        
+        return httpResponse
+        }
     }
+}
 
--- backend/WordsSet/Sources/App/routes.swift
+++ backend/WordsSet/Sources/App/routes.swift
@@ -5,17 +5,6 @@ public func routes(_ router: Router) throws {
 
     let wordController = WordController()
     router.get("/", use: wordController.index)
-    
-    router.post("word") { req -> Future<HTTPResponse> in
-        return try req.content.decode(WordRequest.self).map(to: HTTPResponse.self){
-            WordRequest in print(WordRequest.word)
+    router.post("word", use: wordController.check)
 
-            var httpResponse = HTTPResponse(status: .ok)
-            let response_body = "Thanks for the word: " + WordRequest.word
-            httpResponse.body = HTTPBody(string: response_body)
-            httpResponse.contentType = .html
-            
-            return httpResponse
-        }
-    }
 }
