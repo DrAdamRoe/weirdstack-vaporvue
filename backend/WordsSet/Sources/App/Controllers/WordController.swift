aec03f0e73c7e087c24429e77d2cfb243313edfa
Adam Roe <adam.j.roe@gmail.com>
2019-05-05 16:26:19 +0200

    move index rendering to controller

---
+++ backend/WordsSet/Sources/App/Controllers/WordController.swift
@@ -0,0 +1,23 @@
+import Vapor
+
+/// Controls basic CRUD operations on `Todo`s.
+final class WordController {
+    /// Returns a list of all `Todo`s.
+    func index(_ req: Request) throws -> HTTPResponse {
+        
+        var httpResponse = HTTPResponse(status: .ok)
+        
+        let response_body = """
+        <form method="post" action="/word">
+        word: <input type="text" name="word">
+        <input type="submit" value="Submit">
+        </form>
+        """
+        
+        httpResponse.body = HTTPBody(string: response_body)
+        httpResponse.contentType = .html
+        
+        return httpResponse
+        }
+    }
+
--- backend/WordsSet/Sources/App/routes.swift
+++ backend/WordsSet/Sources/App/routes.swift
@@ -3,27 +3,13 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
 
-    router.get("/") { req -> HTTPResponse in
-
-        var httpResponse = HTTPResponse(status: .ok)
-      
-        let response_body = """
-        <form method="post" action="/word">
-        word: <input type="text" name="word">
-        <input type="submit" value="Submit">
-        </form>
-        """
-        
-        httpResponse.body = HTTPBody(string: response_body)
-        httpResponse.contentType = .html
-        
-        return httpResponse
-    }
-    
+    let wordController = WordController()
+    router.get("/", use: wordController.index)
     
     router.post("word") { req -> Future<HTTPResponse> in
-        return try req.content.decode(WordRequest.self).map(to: HTTPResponse.self){ WordRequest in print(WordRequest.word)
-            
+        return try req.content.decode(WordRequest.self).map(to: HTTPResponse.self){
+            WordRequest in print(WordRequest.word)
+
             var httpResponse = HTTPResponse(status: .ok)
             let response_body = "Thanks for the word: " + WordRequest.word
             httpResponse.body = HTTPBody(string: response_body)
@@ -32,10 +18,4 @@ public func routes(_ router: Router) throws {
             return httpResponse
         }
     }
-    
-    // Example of configuring a controller
-    //    let todoController = TodoController()
-    //    router.get("todos", use: todoController.index)
-    //    router.post("todos", use: todoController.create)
-    //    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
